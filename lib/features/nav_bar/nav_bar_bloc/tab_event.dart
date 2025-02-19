924686acbaa5faeb26eb9f4bc0595c75cc7ceaba
Vinay Raj <89508581+Vinay0141@users.noreply.github.com>
2025-02-19 12:31:19 +0530

    Delete lib directory

--- lib/features/nav_bar/nav_bar_bloc/tab_event.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-abstract class TabEvent {}
-
-class TabChangedEvent extends TabEvent {
-  final int tabIndex;
-
-  TabChangedEvent(this.tabIndex);
-}
--- lib/features/nav_bar/nav_bar_bloc/tab_state.dart
+++
@@ -1,7 +0,0 @@
-abstract class TabState {}
-
-class TabInitialState extends TabState {
-  final int tabIndex;
-
-  TabInitialState({this.tabIndex = 0});
-}
